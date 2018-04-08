a7674899b6201d84f316bef3d0954eb755d79022
Noelia-F <noelia.fronton.g@gmail.com>
2018-04-08 21:49:41 +0200

    functions to add prices not working yet

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 'use strict';
-
+const counter = document.querySelectorAll('.items__number');
+const price = document.querySelectorAll('.item__price');
+let itemCounter = 0;
+let subtotalCounter = 0;
+const itemNumber = document.querySelector('.item--number');
+const list = document.querySelectorAll('.items__checkbock');
+const selectAll = document.querySelector('.button--select-all');
+const unSelectAll = document.querySelector('.button--unselect-all');
+const subtotal = document.querySelector('.item__sum-subtotal');
+const totalPrice = document.querySelector('.item__sum-total');
+const totalButton = document.querySelector('.total--send');
 //Promesa llamada
 fetch('https://raw.githubusercontent.com/Adalab/recipes-data/master/rissoto-setas.json')
   .then(function(response){
@@ -19,9 +29,10 @@ fetch('https://raw.githubusercontent.com/Adalab/recipes-data/master/rissoto-seta
 
 
     //Mostrar productos de la lista
+    currencyHTML = recipe.currency;
     const ingredients = recipe.ingredients;
     for (var i = 0; i < ingredients.length; i++) {
-      ingredientsHTML += '<li class="items"><input class="items__checkbock" type="checkbox" name="" value="">' + '<input class="items__number" type="number" name="" value="" placeholder="1">  <div class="items__description">' + '<h2 class="item item__name">' + ingredients[i].product + '</h2><p class="item item__brand">' + ingredients[i].brand + '</p><p class="item item__quantity">' + ingredients[i].quantity + '</p></div><h3 class="item item__price">' + ingredients[i].price + '€</h3></li>';
+      ingredientsHTML += '<li class="items"><input class="items__checkbock" type="checkbox" name="" value="">' + '<input class="items__number" type="number" name="" value="1" placeholder="1">  <div class="items__description">' + '<h2 class="item item__name">' + ingredients[i].product + '</h2><p class="item item__brand">' + ingredients[i].brand + '</p><p class="item item__quantity">' + ingredients[i].quantity + '</p></div><h3 class="item item__price">' + ingredients[i].price + currencyHTML + '</h3></li>';
     }
     const productsList = document.querySelector('.ingredients__products-list');
     productsList.innerHTML = ingredientsHTML;
@@ -30,12 +41,46 @@ fetch('https://raw.githubusercontent.com/Adalab/recipes-data/master/rissoto-seta
     let shippingPriceHTML;
     shippingPriceHTML = parseFloat(recipe['shipping-cost']).toFixed(2);
     const shippingPrice = document.querySelector('.item__sum-costs');
-    shippingPrice.innerHTML = shippingPriceHTML + '€';
+    shippingPrice.innerHTML = shippingPriceHTML + currencyHTML;
+
+
+    //Añadir un elemento
+    function addPriceArticle(){
+	  itemCounter = 0;
+	  subtotalCounter = 0;
+  	for(var i=0; i < list.length; i++){
+  		if(list[i].checked){
+  			let counterIngredient = counter[i].value;
+  			let priceIngredient = getIngredientPrice(list[i].value);
+  			let sumPrice = parseFloat(counterIngredient) * parseFloat(priceIngredient);
+  			let result = sumPrice.toFixed(2);
+  			price[i].innerHTML = result + currencyHTML;
+  			counter += parseFloat(counterIngredient);
+  			subtotalCounter += parseFloat(result);
+    }
+	}
+		printTotal()
+}
+
+function getIngredientPrice(product){
+	var price = 0;
+	for(var i = 0; i < ingredients.length; i++){
+		if( ingredients[i].product == product){
+			price = ingredients[i].price;
+			break;
+		}
+	}
+	return price;
+}
+
+function printTotal() {
+  itemNumber.innerHTML = counter;
+	subtotal.innerHTML = subtotalCounter.toFixed(2) + currencyHTML;
+	totalPrice.innerHTML = (subtotalCounter + shippingPriceHTML).toFixed(2) + currencyHTML;
+	totalButton.innerHTML = (subtotalCounter + shippingPriceHTML).toFixed(2) + currencyHTML;
+}
 
     //Seleccionar todo y deseleccionar todo
-    const list = document.querySelectorAll('.items__checkbock');
-    const selectAll = document.querySelector('.button--select-all');
-    const unSelectAll = document.querySelector('.button--unselect-all');
     function addAllproducts(){
       for(let i=0; i<list.length; i++){
         if(list[i].type == 'checkbox' ){
