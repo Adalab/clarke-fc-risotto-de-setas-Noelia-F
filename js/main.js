f0e2102c0b2c1d686869cc0f73e73151a9095367
Noelia-F <noelia.fronton.g@gmail.com>
2018-04-08 19:10:00 +0200

    shipping prices and select-unselect ll

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//Promesa llamada
 fetch('https://raw.githubusercontent.com/Adalab/recipes-data/master/rissoto-setas.json')
   .then(function(response){
     return response.json();
@@ -11,19 +12,50 @@ fetch('https://raw.githubusercontent.com/Adalab/recipes-data/master/rissoto-seta
     let currencyHTML = '';
     let ingredientsHTML = '';
 
-    //añadir nombre receta
+    //Mostrar nombre receta
     nameHTML = recipe.name;
     const name = document.querySelector('.title');
     name.innerHTML =  nameHTML;
 
 
-    //añadir productos en lista
+    //Mostrar productos de la lista
     const ingredients = recipe.ingredients;
     for (var i = 0; i < ingredients.length; i++) {
       ingredientsHTML += '<li class="items"><input class="items__checkbock" type="checkbox" name="" value="">' + '<input class="items__number" type="number" name="" value="" placeholder="1">  <div class="items__description">' + '<h2 class="item item__name">' + ingredients[i].product + '</h2><p class="item item__brand">' + ingredients[i].brand + '</p><p class="item item__quantity">' + ingredients[i].quantity + '</p></div><h3 class="item item__price">' + ingredients[i].price + '€</h3></li>';
     }
     const productsList = document.querySelector('.ingredients__products-list');
     productsList.innerHTML = ingredientsHTML;
+
+    //Mostrar gastos envío
+    let shippingPriceHTML;
+    shippingPriceHTML = parseFloat(recipe['shipping-cost']).toFixed(2);
+    const shippingPrice = document.querySelector('.item__sum-costs');
+    shippingPrice.innerHTML = shippingPriceHTML + '€';
+
+    //Seleccionar todo y deseleccionar todo
+    const list = document.querySelectorAll('.items__checkbock');
+    const selectAll = document.querySelector('.button--select-all');
+    const unSelectAll = document.querySelector('.button--unselect-all');
+    function addAllproducts(){
+      for(let i=0; i<list.length; i++){
+        if(list[i].type == 'checkbox' ){
+          list[i].checked = true;
+        }
+      }
+    }
+
+    function removeAllproducts(){
+      for(let i=0; i<list.length; i++){
+        if(list[i].type == 'checkbox' ){
+          list[i].checked = false;
+        }
+      }
+    }
+
+    selectAll.addEventListener('click', addAllproducts);
+    unSelectAll.addEventListener('click', removeAllproducts);
+
+
 
 
   });
